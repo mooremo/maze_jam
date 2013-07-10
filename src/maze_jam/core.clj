60a2a25dee24a0ab2c658c02407027c1f4e7de70
Matt Moore <mmoore@groupon.com>
2013-07-10 14:12:41 -0500

    just pusing bcz

--- src/maze_jam/core.clj
+++ src/maze_jam/core.clj
@@ -1,6 +1,24 @@
 (ns maze-jam.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-maze-template [width height]
+  (vec (for [x (range width)]
+    (vec (for [y (range height)]
+      -1))))
+  )
+
+(defn possible-neighbours [x y]
+  (distinct (for [delta-x [-1 0 1]
+                  delta-y [-1 0 1]]
+              [(+ delta-x x) (+ delta-y y)])))
+
+;todo remove values that contain negative coordinates
+(defn neighbours [x y]
+  (for [[x y] (possible-neighbours x y)
+        :when (and (<= 0 x) (<= 0 y))]
+    [x y])
+  )
+
+(defn unvisited-neighbours [x y maze]
+  (for [neighbour (neighbours x y)]
+    :when (> 0 (y (x maze))))
+  )
